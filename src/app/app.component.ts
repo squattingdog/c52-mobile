0494913e746d2018fd36147b8b01da19cfa4ac4a
Corey Pitzo <corey.pitzo@salesforce.com>
2018-05-31 16:19:31 -0400

    implemented sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,44 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
 import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { App, MenuController, Nav, Platform } from 'ionic-angular';
 import { HomePage } from '../pages/home/home';
+import { CampaignsPage } from '../pages/campaigns/campaigns';
+
 @Component({
-  templateUrl: 'app.html'
+	templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+	pages;
+	rootPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+	@ViewChild(Nav) nav: Nav;
+
+	constructor(
+		private app: App,
+		private platform: Platform,
+		private menu: MenuController,
+		private statusBar: StatusBar,
+	) {
+		this.initializeApp();
+
+		// set our app's pages
+		this.pages = [
+			{ title: 'Home', component: HomePage, icon: 'home' },
+			{ title: 'Campaigns', component: CampaignsPage, icon: 'grid' },
+		];
+
+		this.rootPage = HomePage;
+	}
+
+	initializeApp() {
+		this.platform.ready().then(() => {
+			this.statusBar.styleDefault();
+		});
+	}
+
+	openPage(page) {
+		this.menu.close();
+		this.nav.setRoot(page.component);
+	}
 }
-
