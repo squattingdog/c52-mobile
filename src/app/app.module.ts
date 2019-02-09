a6d4c19ff7323869348bb3c2439dfe47049260d4
SquattingDog <squattingdog@users.noreply.github.com>
2019-02-08 20:02:08 -0500

    View shifts (#2)
    * view shifts
    
    * able to sign up for a shift

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,32 +9,43 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { CampaignsPage } from './../pages/campaigns/campaigns';
+import { JobsPage } from '../pages/jobs/jobs';
+import { ShiftsPage } from '../pages/shifts/shifts';
 
 import { CampaignService } from './../providers/campaign-service';
 import { HttpClientModule } from '@angular/common/http';
+
+import { Toast } from '@ionic-native/toast/ngx';
 
 @NgModule({
   declarations: [
     MyApp
     ,HomePage
     ,CampaignsPage
+    ,JobsPage
+    ,ShiftsPage
   ]
   ,imports: [
     BrowserModule
     ,HttpClientModule
     ,IonicModule.forRoot(MyApp)
     ,IonicPageModule.forChild(CampaignsPage)
+    ,IonicPageModule.forChild(JobsPage)
+    ,IonicPageModule.forChild(ShiftsPage)
   ]
   ,bootstrap: [IonicApp]
   ,entryComponents: [
     MyApp
     ,HomePage
+    ,JobsPage
+    ,ShiftsPage
   ]
   ,providers: [
     StatusBar
     ,SplashScreen
     ,{provide: ErrorHandler, useClass: IonicErrorHandler}
     ,CampaignService
+    ,Toast
   ]
 })
 
--- src/providers/campaign-service.ts
+++ src/providers/campaign-service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs/observable";
 import { map, catchError } from 'rxjs/operators';
+import * as moment from 'moment';
 
 /*
   Generated class for the CampaignService provider.
@@ -11,18 +12,79 @@ import { map, catchError } from 'rxjs/operators';
 */
 @Injectable()
 export class CampaignService {
-    //private config: Config;
+    private baseUrl = "https://c52-node-dev.herokuapp.com";
+    // private baseUrl = "http://localhost:5000";
+    // private config: Config;
 
     constructor(private http:HttpClient) { }
 
     public getCampaigns():Observable<CampaignItem[]> {
         console.log('getting campaigns');
-        return this.http.get('http://localhost:5000/api/v1/campaigns').pipe(
+        return this.http.get(`${this.baseUrl}/api/v1/campaigns`).pipe(
             map(
                 this.extractCampaignData,
                 catchError(this.handleError)
             )                
         );
+    }
+
+    public getJobs(campaignId: string):Observable<JobItem[]> {
+        return this.http.get(`${this.baseUrl}/api/v1/campaign/${campaignId}/jobs/`).pipe(
+            map(
+                this.extractJobData,
+                catchError(this.handleError)
+            )
+        );
+    }
+
+    public getShifts(campaignId: string, jobId: string): Observable<ShiftItem[]> {
+        return this.http.get(`${this.baseUrl}/api/v1/campaign/${campaignId}/job/${jobId}/shifts/`).pipe(
+            map(
+                this.extractShiftData,
+                catchError(this.handleError)
+            )
+        );
+    }
+
+    public volunteerForShift(campaign: CampaignItem, job: JobItem, shift: ShiftItem, contactId: string): Observable<any> {
+        let uri = `${this.baseUrl}/api/v1/campaign/${campaign.campaignId}/job/${job.jobId}/shift/${shift.shiftId}/volunteer`;
+        let body = {
+            "startDateTime": shift.startDateTime,
+            "contactId": contactId
+        };
+        return this.http.post(uri, body).pipe(
+            map(
+                res => {
+                    let objectRes: any = res;
+                    return objectRes;
+                },
+                catchError(this.handleError)
+            )
+        );
+    }
+
+    private extractShiftData(res: Response): ShiftItem[] {
+        let objects: any = res;
+        let shifts: ShiftItem[] = [];
+        for(let i = 0; i < objects.length; i++) {
+            let item: ShiftItem = <ShiftItem>objects[i];
+            item.displayStartDate = moment(item.startDateTime).format('MMMM Do YYYY');
+            item.displayEndDate = moment(item.endDateTime).format('MMMM Do YYYY');
+            item.displayStartTime = moment(item.startDateTime).format('h:mm a');
+            item.displayEndTime = moment(item.endDateTime).format('h:mm a');
+            shifts.push(item);
+        }
+        return shifts;
+    }
+
+    private extractJobData (res: Response): JobItem[] {
+        let objects:any = res;
+        let jobs: JobItem[] = [];
+        for(let i = 0; i < objects.length; i++) {
+            let item: JobItem = <JobItem>objects[i];            
+            jobs.push(item);
+        }
+        return jobs;
     }
 
     private extractCampaignData (res:Response): CampaignItem[] {
@@ -49,6 +111,19 @@ export class CampaignService {
         }
         return Observable.throw(errMsg);
     }
+
+    public groupBy(list: Array<ShiftItem>, keyGetter: any): Map<string, ShiftItem[]> {
+        const map = new Map<string, ShiftItem[]>();
+        list.forEach((item) => {
+            const key = keyGetter(item);
+            if(map.has(key)) {
+                map.get(key).push(item);
+            } else {
+                map.set(key, new Array<ShiftItem>(item));
+            }
+        });
+        return map;
+    }
 }
 
 export class CampaignItem {
@@ -64,5 +139,56 @@ export class CampaignItem {
         this.logoUrl = theLogoUrl;
         this.campaignId = theCampaignId;
         this.id = theId;
+    }
+}
+
+export class JobItem {
+    public jobId: string;
+    public name: string;
+    public description: string;
+    public skills: string;
+    public ongoing: boolean;
+    public numberOfVolunteersStillNeeded: number;
+    public numberOfShifts: number;
+    public active: boolean;
+    public location: LocationModel;
+    public campaignId: string;
+    public displayOnWebsite: boolean;
+}
+
+export class ShiftItem {
+    public name: string;
+    public shiftId: string;
+    public jobId: string;
+    public active: boolean;
+    public location: LocationModel;
+    public endDateTime: Date;
+    public numberOfVolunteersStillNeeded: number;
+    public desiredNumberOfVolunteers: number;
+    public totalVolunteers: number;
+    public startDateTime: Date;
+    public duration: number;
+    public description: string;
+    public displayStartDate: string;
+    public displayEndDate: string;
+    public displayStartTime: string;
+    public displayEndTime: string;
+}
+
+export class LocationModel {
+    zip: string;
+    street: string;
+    state: string;
+    information: string;
+    geocode: string;
+    city: string;
+
+    constructor(theZip: string, theStreet: string, theState: string, theInfo: string, theGeo: string, theCity: string) {
+        this.zip = theZip;
+        this.street = theStreet;
+        this.state = theState;
+        this.information = theInfo;
+        this.geocode = theGeo;
+        this.city = theCity;
     }
 }
